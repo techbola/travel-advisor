2730996eba157b2e803ea2abe5375681c7003b54
Adebola <bfagbowore@gmail.com>
2021-12-01 00:48:23 +0100

    place details design

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
   const [places, setPlaces] = useState([]);
 
   const [coordinates, setCoordinates] = useState({});
-  const [bounds, setBounds] = useState(null);
+  const [bounds, setBounds] = useState({});
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
