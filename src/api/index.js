f381076c1bd111004f69758991179273e94f058c
Adebola <bfagbowore@gmail.com>
2021-12-01 00:17:39 +0100

    rapid api setup, fetch restaurants of a location using lat and lng

---
+++ src/api/index.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const URL =
+  "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary";
+
+export const getPlacesData = async (sw, ne) => {
+  try {
+    const {
+      data: { data },
+    } = await axios.get(URL, {
+      params: {
+        bl_latitude: sw.lat,
+        tr_latitude: ne.lat,
+        bl_longitude: sw.lng,
+        tr_longitude: ne.lng,
+      },
+      headers: {
+        "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
+        "x-rapidapi-key": "5fba8d2a6bmsha63b7b91abd2db3p1c27a0jsn4b84344d652d",
+      },
+    });
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
