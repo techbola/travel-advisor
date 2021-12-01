07638d54fe4159440356a98e9a5cad4af0be838c
Adebola <bfagbowore@gmail.com>
2021-12-01 01:06:59 +0100

    show places on the map

--- src/App.js
+++ src/App.js
@@ -40,6 +40,7 @@ const App = () => {
             setCoordinates={setCoordinates}
             setBounds={setBounds}
             coordinates={coordinates}
+            places={places}
           />
         </Grid>
       </Grid>
